a2ce655c963bb5b3d9013489541e054b20211019
wanghuali20 <1694631707@qq.com>
2024-07-03 14:33:59 +0800

    第三次提交

--- labixiaoxin/js/nav.js
+++ labixiaoxin/js/nav.js
@@ -1,7 +1,15 @@
+//$(".menu2").mouseenter(() => {
+    //$(".menu2 ul").slideDown("1000");
+//})
+$//(".menu2").mouseleave(() => {
+    //console.log(234);
+    //$(".menu2>ul").slideUp("1000");
+//})
+
 $(".menu2").mouseenter(() => {
     $(".menu2 ul").slideDown("1000");
-})
-$(".menu2").mouseleave(() => {
-    console.log(234);
-    $(".menu2>ul").slideUp("1000");
-})
+    })
+    $(".menu2").mouseleave(() => {
+    let ul = $(".menu2>ul");
+    ultrafina(ul);
+});
--- labixiaoxin/js/validateForm.js
+++ labixiaoxin/js/validateForm.js
@@ -3,7 +3,9 @@ function validateForm() {
     var user = document.querySelector("#user").value;
     var psd = document.querySelector("#psd").value;
     var regUser = /^[a-z0-9]{8,12}$/;
+    //用户名必须是8到12位的字母或数字。
     var regPsd = /^[a-z0-9]{6,10}$/;
+    //密码必须是6到10位的字母或数字。
     if (!regUser.test(user)) {
         alert("账号只能8到12位");
         return false;
@@ -26,3 +28,22 @@ form.addEventListener("submit", function (e) {
         location.href = "./index.html"
     };
 })
+
+//密码显示与隐藏功能
+function togglePassword() {
+    var passwordInput = document.getElementById ('password');
+    var eye = document.querySelector ('.eye');
+
+    if (flag) {
+        passwordInput.type  = 'password';
+        eye.innerHTML  = '<i class="fas fa-eye"></i>';
+        flag = false;
+    } else {
+        passwordInput.type  = 'text';
+        eye.innerHTML  = '<i class="fas fa-eye-slash"></i>';
+        flag = true;
+    }
+}
+
+// 初始化标志变量
+flag = false;
